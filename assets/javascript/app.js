6172b8f3d6fe3bfaad20bb4b8f2911dc96f1586e
Grady <Gradyhouston@gmail.com>
2018-03-25 21:55:47 -0400

    adding completed project

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+// JavaScript30 - Day 1
+// JavaScript DrumKit
+
+
+  function playSound(event) {
+      var audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+      var key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+      if (!audio) return; // Will stop the function from running all together
+      audio.currentTime = 0; // Rewind to start
+      audio.play();
+      key.classList.add("playing");
+  }
+
+  function removeTransition (event) {
+    if (event.propertyName !== "transform") return // skip if not transform
+    this.classList.remove("playing");
+  }
+
+  var keys = document.querySelectorAll(".key");
+  keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+  window.addEventListener("keydown", playSound);
